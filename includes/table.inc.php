15a4d0eb919132c28f4357355bf90e352c1538ef
Nathan Stéwill LOUFOUA <127401929+LNStewill@users.noreply.github.com>
2023-10-26 15:39:49 +0200

    AJout du php et dynamisation du tableau

---
+++ includes/table.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$_topTenLang = array(
+            array("Python" => "Web & application"),
+            array("C++" => "Application"),
+            array("Node js" => "Application"),
+            array("PHP" => "Web & application")
+     );
+     
+     //print_r($_topTenLang);
+    foreach ($_topTenLang as $_index => $_key) {
+    print '<tr>'.'<td>'.$_index.'</td>';
+    foreach ($_key as $_new_index => $_valeur) {
+        print '<td>'.$_new_index.'</td>'.'<td>'.$_valeur.'</td>'.'</tr>'."\n";
+    }
+}
---
+++ index.php
@@ -0,0 +1,57 @@
+<!--head-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="classement des langages web">
+    <title>Spécial arrow</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/arrow.css">
+</head><body>
+    <header>
+        <h1>Spécial arrow <i aria-hidden="true" class="arrow-left"></i></h1>
+    </header>
+    <main>
+        <div class="section" role="table" aria-label="table-info">
+            <table>
+                <caption>Le top 10 des langages</caption>
+                <thead>
+                    <tr>
+                        <th role="columnheader">Classement</th>
+                        <th>Langage</th>
+                        <th>Type</th>
+                    </tr>
+                </thead>
+                <!--affichage des données-->
+
+                
+                <tbody>
+                    <?php    
+                    
+                    include_once("./includes/table.inc.php");
+                    ?>                                
+                </tbody>
+                <!--partie footer-->
+                <tfoot>
+                    <tr>
+                        <th>Classement</th>
+                        <th>Langage</th>
+                        <th>Type</th>
+                    </tr>
+                </tfoot>
+            </table>
+
+        </div>
+    <footer>
+        <p>&copy; - LNStewill&nbsp;  <time datetime="10-18-2023">2023</time>  </p>
+    </footer>    
+</main>
+  
+</body>
+</html>
